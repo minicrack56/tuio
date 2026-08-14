b49e334d60a4d5241a316528fba0d6ff238236b5
minicrack56 <66340662+minicrack56@users.noreply.github.com>
2026-08-14 21:33:14 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
---- src/main.tsx (原始)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -7,7 +6,6 @@ import App from "./App.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
 
 
-+++ src/main.tsx (修改后)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
